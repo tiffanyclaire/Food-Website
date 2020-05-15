994af524026fe4a1d8d22290168b7b0ad67dbbc8
tiffanyclaire <tiffanyclairefarthing@gmail.com>
2020-05-15 16:04:24 +0100

    First  push of project files

---
+++ project0/Includes/header.php
@@ -0,0 +1,17 @@
+
+	<!--Navigation-->
+<header>
+	<div class="container">
+	
+	<a href="index.html"> <img id="logo" src="Images/better-food-logo.png"> </a>
+	
+	<nav id="navbar">
+		<ul>
+			<li> <a href="About.html">About</a> </li>
+			<li> <a href="ShopLocal.html">Shop Local</a> </li>
+			<li> <a href="Events.html">Events</a> </li>
+			<li> <a href="GetTheApp.html">Get The App</a> </li>
+		</ul>
+		
+
+	</nav>
